061141e326cb16d00af7364d68de378b131b1a79
GuoMin <guomin0999@gmail.com>
2016-03-28 18:02:12 +0800

    Initial commit

---
+++ app/src/main/java/cn/guomin0999/nrecord/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.guomin0999.nrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
