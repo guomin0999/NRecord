0e34baf4fa4b41e865238119fc64f57461173938
GuoMin <guomin0999@gmail.com>
2016-03-29 15:37:28 +0800

    copy

--- app/src/main/java/cn/guomin0999/nrecord/MainActivity.java
+++ app/src/main/java/cn/guomin0999/nrecord/MainActivity.java
@@ -1,12 +1,24 @@
 package cn.guomin0999.nrecord;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cn.guomin0999.library.AudioRecordingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AudioRecordingActivity.class);
+                //单位转毫秒
+                startActivityForResult(intent.putExtra("minDuration", 2000).putExtra("maxDuration", 60 * 1000), 0);
+            }
+        });
     }
 }
